46a016c27826dba7a51aa932e8fca6b645226454
JaroFuska <jarofuska@gmail.com>
2016-12-22 18:34:43 +0100

    Add files via upload

--- src/laravel/app/Http/Controllers/DiaryController.php
+++ src/laravel/app/Http/Controllers/DiaryController.php
@@ -30,6 +30,11 @@ class DiaryController extends Controller {
 
 	}
 
+	/**
+	 * Upload data to DB and redirect to running_plan
+	 *
+	 * @return running_plan
+	 */
 	public function create(){
 		$userID = Auth::user()->id;
 		(float)$distance = Input::get('distance');
@@ -37,7 +42,19 @@ class DiaryController extends Controller {
 		(int)$mood = Input::get('mood');
 		$userRunningPlanID = 2;
 
+		if (!($distance > 0)){
+			return view('diary')->withErrors('Zle zadana vzdialenost');
+		}
+		$mytime = \Carbon\Carbon::now();
+		if ($date > $mytime){
+			return view('diary')->withErrors('Nespravne zadany datum');
+		}
+
 		$userRunPlans = UserRunningPlan::where('user_id', $userID)->orderBy('running_plan_id')->lists('running_plan_id');
+		if (empty($userRunPlans)){
+			return view('diary')->withErrors('Nemas ziadne aktivne bezecke plany');
+		}
+		DB::table('user_running_plans')->where('user_id', $userID)->increment('total_distance', $distance);
 		foreach ($userRunPlans as $urp) {
 			RunningData::create([
 			'user_id' => $userID,
@@ -48,8 +65,7 @@ class DiaryController extends Controller {
 			]);
 		}
 
-		
-		return view('diary');
+		return redirect('running_plan')->with('status', 'Záznam z behu pridaný!');
 	}
 
 }
