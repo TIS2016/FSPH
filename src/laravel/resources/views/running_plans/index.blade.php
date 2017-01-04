8a210f49d97da7c0e4afe638c071935456453a7e
JaroFuska <jarofuska@gmail.com>
2017-01-04 22:15:47 +0100

    oznam o pridani zaznamu

--- src/laravel/resources/views/running_plans/index.blade.php
+++ src/laravel/resources/views/running_plans/index.blade.php
@@ -7,6 +7,11 @@
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
+        @if (session('status'))
+                    <div class="alert alert-success">
+                        {{ session('status') }}
+                    </div>
+                @endif
             <div class="panel panel-default">
                 <div class="panel-heading">Zoznam bežeckých plánov</div>
                 <div class="panel-body">
