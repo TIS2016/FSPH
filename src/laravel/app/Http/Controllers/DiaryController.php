daea074ab3643ac707b000f5cad1efbb271b873e
JaroFuska <jarofuska@gmail.com>
2017-01-04 22:10:38 +0100

    pridany redirect na plán a kontrola aktivity plánu

--- src/laravel/app/Http/Controllers/DiaryController.php
+++ src/laravel/app/Http/Controllers/DiaryController.php
@@ -11,14 +11,11 @@ use Illuminate\Foundation\Bus\DispatchesCommands;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Support\Facades\Input;
-
 class DiaryController extends Controller {
-
 	public function __construct()
 	{
 		$this->middleware('auth');
 	}
-
 	/**
 	 * Show the application dashboard to the user.
 	 *
@@ -27,17 +24,36 @@ class DiaryController extends Controller {
 	public function index()
 	{
 		return view('diary');
-
 	}
-
+	/**
+	 * Upload data to DB and redirect to running_plan
+	 *
+	 * @return running_plan
+	 */
 	public function create(){
 		$userID = Auth::user()->id;
 		(float)$distance = Input::get('distance');
 		$date = Input::get('date');
 		(int)$mood = Input::get('mood');
 		$userRunningPlanID = 2;
-
-		$userRunPlans = UserRunningPlan::where('user_id', $userID)->orderBy('running_plan_id')->lists('running_plan_id');
+		if (!($distance > 0)){
+			return view('diary')->withErrors('Zle zadaná vzdialenosť');
+		}
+		$mytime = \Carbon\Carbon::now();
+		if ($date > $mytime){
+			return view('diary')->withErrors('Nesprávne zadaný dátum');
+		}
+		$userRunPlans =  DB::table('user_running_plans')
+								->join('running_plans', 'user_running_plans.running_plan_id', '=', 'running_plans.id')	
+								->select('running_plans.id')	
+								->where('running_plans.end', '>', $mytime)
+								->where('user_id', $userID)
+								->lists('running_plans.id');
+		var_dump($userRunPlans);
+		if (empty($userRunPlans)){
+			return view('diary')->withErrors('Nemáš žiadne aktívne bežecké plány');
+		}
+		DB::table('user_running_plans')->where('user_id', $userID)->increment('total_distance', $distance);
 		foreach ($userRunPlans as $urp) {
 			RunningData::create([
 			'user_id' => $userID,
@@ -47,10 +63,6 @@ class DiaryController extends Controller {
 			'distance' => $distance,
 			]);
 		}
-
-		
-		return view('diary');
+		return redirect('running_plan')->with('status', 'Záznam z behu pridaný!');
 	}
-
 }
-
